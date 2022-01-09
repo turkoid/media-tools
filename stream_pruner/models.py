27ab2ffb16525813562ae211ae3bd0b83d17ae9d
John Ross <jlross42@gmail.com>
2022-01-08 16:31:38 -0800

    refactor to make pruner generic

--- stream_pruner/models.py
+++ stream_pruner/models.py
@@ -1,3 +1,6 @@
+import itertools
+
+
 class Track:
     def __init__(self, track_json: dict):
         self.raw: dict = track_json
@@ -5,16 +8,26 @@ class Track:
         self.type: str = track_json["type"]
         self.properties: dict = track_json["properties"]
         self.default: bool = self.properties.get("default_track", False)
-        self.enabled: bool = self.properties.get("enabled_track", False)
+        self.enabled: bool = self.properties.get("enabled_track", True)
         self.original: bool = self.properties.get("flag_original", False)
         self.forced_prop: bool = self.properties.get("forced_track", False)
         self.language: str = self.properties.get("language", "und")
         self.track_name: str = self.properties.get("track_name", "")
-        self.codec = self.properties.get("codec_id", "")[2:]
+        self.short_type, self.codec = self.properties.get("codec_id", "?-").split("-")
 
     @property
     def forced(self) -> bool:
         return self.forced_prop or "forced" in self.track_name.lower()
+
+    def lang_code(self, undefined_lang: str) -> str:
+        return undefined_lang if self.language == "und" else self.language
+
+    def __str__(self):
+        track_name = self.track_name or "<und>"
+        return f"[{self.id}][{self.codec}].{self.language}: {track_name}"
+
+    def __repr__(self):
+        return str(self)
 
 
 class VideoTrack(Track):
@@ -22,12 +35,8 @@ class VideoTrack(Track):
 
 
 class AudioTrack(Track):
-    def __str__(self):
-        track_name = self.track_name or "<und>"
-        return f"[{self.id}][{self.codec}].{self.language}: {track_name} (Enabled: {self.enabled}, Original: {self.original}), Default: {self.default}, Forced: {self.forced}"
-
     def __repr__(self):
-        return str(self)
+        return f"{self} (enabled={self.enabled}, original={self.original}, default={self.default}, forced={self.forced})"
 
 
 class SubtitleTrack(Track):
@@ -39,18 +48,14 @@ class SubtitleTrack(Track):
 
     @property
     def hearing_impaired(self) -> bool:
-        return self.hearing_impaired_prop or "SDH" in self.track_name.lower()
+        return self.hearing_impaired_prop or "sdh" in self.track_name.lower()
 
     @property
     def vobsub(self) -> bool:
         return self.codec == "VOBSUB"
 
-    def __str__(self):
-        track_name = self.track_name or "<und>"
-        return f"[{self.id}].{self.language}: {track_name} (Enabled: {self.enabled}, Original: {self.original}, Default: {self.default}, Forced: {self.forced})"
-
     def __repr__(self):
-        return str(self)
+        return f"{self} (enabled={self.enabled}, original={self.original}, default={self.default}, forced={self.forced}, vobsub={self.vobsub}, sdh={self.hearing_impaired})"
 
 
 class MkvData:
@@ -59,8 +64,7 @@ class MkvData:
         self.raw: dict = raw_data
         self.video_tracks: list[VideoTrack] = []
         self.audio_tracks: list[AudioTrack] = []
-        self.subtitle_tracks_list: list[SubtitleTrack] = []
-        self.subtitle_tracks: dict[str, list[SubtitleTrack]] = {}
+        self.subtitle_tracks: list[SubtitleTrack] = []
         self.other_tracks: list[Track] = []
         for track in sorted(self.raw["tracks"], key=lambda t: t["id"]):
             if track["type"] == "video":
@@ -68,13 +72,16 @@ class MkvData:
             elif track["type"] == "audio":
                 self.audio_tracks.append(AudioTrack(track))
             elif track["type"] == "subtitles":
-                sub_track = SubtitleTrack(track)
-                self.subtitle_tracks_list.append(sub_track)
-                self.subtitle_tracks.setdefault(sub_track.language, []).append(
-                    sub_track
-                )
+                self.subtitle_tracks.append(SubtitleTrack(track))
             else:
                 self.other_tracks.append(Track(track))
 
     def is_valid(self) -> bool:
         return bool(self.video_tracks and self.audio_tracks and self.subtitle_tracks)
+
+    @property
+    def tracks(self) -> list[VideoTrack | AudioTrack | SubtitleTrack]:
+        all_tracks = list(
+            itertools.chain(self.video_tracks, self.audio_tracks, self.subtitle_tracks)
+        )
+        return all_tracks
--- stream_pruner/stream_pruner.py
+++ stream_pruner/stream_pruner.py
@@ -1,11 +1,14 @@
+import itertools
 import logging
 import os
-from typing import Optional
+from typing import Optional, TypeVar
 
 from core.config import Config
 from core.tool import Tool
-from core.utils import run_process, log_file_header, validate_paths
-from stream_pruner.models import SubtitleTrack, MkvData, AudioTrack, VideoTrack
+from core.utils import run_process, log_file_header, validate_paths, identify
+from stream_pruner.models import SubtitleTrack, MkvData, AudioTrack, VideoTrack, Track
+
+T = TypeVar("T", bound=Track)
 
 
 class StreamPruner(Tool):
@@ -18,6 +21,12 @@ class StreamPruner(Tool):
             self.input_directory = self.input
         else:
             self.input_directory = os.path.dirname(self.input)
+        self.video_lang_codes = self._normalize_lang_codes(parsed_args.video_lang)
+        self.audio_lang_codes = self._normalize_lang_codes(parsed_args.audio_lang)
+        self.subtitle_lang_codes = self._normalize_lang_codes(parsed_args.subtitle_lang)
+        self.prefer_text_subtitles: bool = parsed_args.prefer_text_subtitles
+        self.prefer_sdh_subtitles: bool = parsed_args.prefer_sdh_subtitles
+        self.undefined_lang: str = parsed_args.undefined_lang.lower() or "und"
         self.output_directory: Optional[str] = parsed_args.output_directory
         output_path = os.path.join(
             self.input_directory, self.output_directory or "pruned"
@@ -25,11 +34,14 @@ class StreamPruner(Tool):
         self.output_path = os.path.abspath(output_path)
         self.validate()
 
+    def _normalize_lang_codes(self, lang_codes: list[str]) -> list[str]:
+        return list(dict.fromkeys(lang.lower() for lang in lang_codes))
+
     def validate(self):
         validate_paths(self.config.mkvmerge, self.input)
         if os.path.samefile(self.input_directory, self.output_path):
             raise ValueError(
-                f"output directory must be different then the input directory"
+                "output directory must be different then the input directory"
             )
 
     def run(self):
@@ -37,88 +49,136 @@ class StreamPruner(Tool):
         os.makedirs(self.output_path, exist_ok=True)
         self.prune_files(media_files)
 
+    def _filter_tracks(self, tracks: list[T], lang_codes: list[str]) -> list[T]:
+        filtered_tracks = [
+            track
+            for track in tracks
+            if not lang_codes or track.lang_code(self.undefined_lang) in lang_codes
+        ]
+        return filtered_tracks
+
+    def _sort_tracks(self, tracks: list[T], lang_codes: list[str]) -> list[T]:
+        track_lang_codes = [t.lang_code(self.undefined_lang) for t in tracks]
+        lang_codes = list(dict.fromkeys(lang_codes + track_lang_codes))
+        sorted_tracks = sorted(
+            tracks, key=lambda t: lang_codes.index(t.lang_code(self.undefined_lang))
+        )
+        return sorted_tracks
+
     def _filter_video_tracks(self, data: MkvData) -> list[VideoTrack]:
-        tracks = []
-        for track in data.video_tracks:
-            tracks.append(track)
-            logging.info(f"++V {track}")
+        tracks = self._filter_tracks(data.video_tracks, self.video_lang_codes)
+        tracks = self._sort_tracks(tracks, self.video_lang_codes)
         return tracks
 
     def _filter_audio_tracks(self, data: MkvData) -> list[AudioTrack]:
-        first_track = data.audio_tracks[0]
-        tracks = [first_track]
-        logging.info(f"++A {first_track}")
-        for track in data.audio_tracks[1:]:
-            logging.info(f"--A {track}")
+        tracks = self._filter_tracks(data.audio_tracks, self.audio_lang_codes)
+        tracks = sorted(
+            tracks,
+            key=lambda t: (t.enabled, t.original, t.default),
+            reverse=True,
+        )
+        tracks = self._sort_tracks(tracks, self.audio_lang_codes)
         return tracks
 
     def _filter_subtitle_tracks(
         self, data: MkvData, default_lang: str
     ) -> list[SubtitleTrack]:
-        subtitles: dict[str, dict[str, Optional[SubtitleTrack]]] = {}
-        for lang in ["eng", "kor"]:
-            lang_subs = subtitles.setdefault(lang, {"forced": None, "full": None})
-            for track in data.subtitle_tracks.get(lang, []):
-                if not lang_subs["forced"] or not lang_subs["full"]:
-                    st_type: str = "forced" if track.forced else "full"
-                    if not lang_subs[st_type]:
-                        logging.info(f"++S {track}")
-                        lang_subs[st_type] = track
-                        continue
-                logging.info(f"--S {track}")
-        if default_lang == "eng":
-            track_order = [
-                subtitles["eng"]["forced"],
-                subtitles["eng"]["full"],
-                subtitles["kor"]["forced"],
-                subtitles["kor"]["full"],
-            ]
-        else:
-            track_order = [
-                subtitles["eng"]["full"],
-                subtitles["eng"]["forced"],
-                subtitles["kor"]["full"],
-                subtitles["kor"]["forced"],
-            ]
-        tracks = [t for t in track_order if t]
+        tracks = self._filter_tracks(data.subtitle_tracks, self.subtitle_lang_codes)
+        lang_codes = self.subtitle_lang_codes or list(
+            dict.fromkeys(t.lang_code(self.undefined_lang) for t in tracks)
+        )
+        prefer_forced = lang_codes[0] == default_lang
+        tracks = sorted(
+            tracks,
+            key=lambda t: (
+                t.enabled,
+                t.original,
+                t.forced == prefer_forced,
+                t.default,
+                t.vobsub == self.prefer_text_subtitles,
+                t.hearing_impaired == self.prefer_sdh_subtitles,
+            ),
+            reverse=True,
+        )
+        tracks = self._sort_tracks(tracks, self.subtitle_lang_codes)
         return tracks
 
+    def _output_track_operations(
+        self, old_tracks: list[Track], new_tracks: list[Track]
+    ):
+        new_track_order = {t.id: (i, t) for i, t in enumerate(new_tracks)}
+        old_track_order = {
+            t.id: (i, t)
+            for i, t in enumerate(t for t in old_tracks if t.id in new_track_order)
+        }
+
+        for track_id, (old_index, track) in old_track_order.items():
+            move = " "
+            if track_id in new_track_order:
+                new_index, new_track = new_track_order[track_id]
+                op = "++"
+                if new_index > old_index:
+                    move = "↑"
+                elif new_index < old_index:
+                    move = "↓"
+            else:
+                op = "--"
+            logging.info(f"{op}{move}{track.short_type} {track}")
+        if len(old_tracks) == len(new_tracks) and list(new_track_order.keys()) == list(
+            old_track_order.keys()
+        ):
+            logging.warning("All tracks will be the same")
+
     def prune_media(self, media_file: str):
-        data = MkvData(media_file, self.config)
+        data = MkvData(media_file, identify(self.config.mkvmerge, media_file))
         log_file_header(media_file)
         if not data.is_valid():
             track_info = [
                 f"V:{bool(data.video_tracks)}",
                 f"A:{bool(data.audio_tracks)}",
-                f"S:{'eng' in data.subtitle_tracks}",
+                f"S:{bool(data.subtitle_tracks)}",
             ]
             logging.warning(f"missing tracks {' '.join(track_info)}")
             return
+        video_tracks = self._filter_video_tracks(data)
         audio_tracks = self._filter_audio_tracks(data)
-        subtitle_tracks = self._filter_subtitle_tracks(data, audio_tracks[0].language)
+        default_lang = (
+            self.audio_lang_codes[0]
+            if self.audio_lang_codes
+            else audio_tracks[0].language
+        )
+        subtitle_tracks = self._filter_subtitle_tracks(data, default_lang)
+        if not video_tracks or not audio_tracks:
+            logging.error(f"No video and/or audio tracks found")
+            return
+        new_tracks = list(itertools.chain(video_tracks, audio_tracks, subtitle_tracks))
+        self._output_track_operations(data.tracks, new_tracks)
         pruned_file = os.path.join(self.output_path, os.path.basename(media_file))
         args = [
             self.config.mkvmerge,
             "-o",
             pruned_file,
+            "--video-tracks",
+            ",".join(str(t.id) for t in video_tracks),
             "--audio-tracks",
             ",".join(str(t.id) for t in audio_tracks),
-            "--default-track",
-            f"{audio_tracks[0].id}:1",
-            "--subtitle-tracks",
-            ",".join(str(t.id) for t in subtitle_tracks),
         ]
-        for i, track in enumerate(subtitle_tracks):
-            default_flag = 1 if i == 0 else 0
-            forced_flag = 1 if track.forced else 0
+        if subtitle_tracks:
+            args.extend(
+                [
+                    "--subtitle-tracks",
+                    ",".join(str(t.id) for t in subtitle_tracks),
+                ]
+            )
+        short_type = None
+        for track in new_tracks:
+            default_flag = 1 if short_type != track.short_type else 0
+            short_type = track.short_type
             args.extend(["--default-track", f"{track.id}:{default_flag}"])
-            args.extend(["--forced-track", f"{track.id}:{forced_flag}"])
-            if track.forced:
-                args.extend(["--track-episode", f"{track.id}:Forced"])
         args.extend(
             [
                 "--track-order",
-                ",".join(f"0:{track.id}" for track in subtitle_tracks),
+                ",".join(f"0:{t_id}" for t_id in new_tracks.keys()),
             ]
         )
         args.append(media_file)
@@ -146,13 +206,47 @@ class StreamPruner(Tool):
     @staticmethod
     def create_parser(subparsers):
         parser = subparsers.add_parser(
-            "prune", description="prune unneeded tracks from media"
+            "prune", description="prunes and reorders tracks from media"
         )
         parser.add_argument(
             "--input",
             "-i",
             required=True,
             help="path to directory or file. If directory, the script will handle only video files",
+        )
+        parser.add_argument(
+            "-v",
+            "--video-lang",
+            nargs="*",
+            help="list of language codes to keep and in which order. (uses 3 letter ISO 639-3 codes)",
+        )
+        parser.add_argument(
+            "-a",
+            "--audio-lang",
+            nargs="*",
+            help="list of language codes to keep and in which order. (uses 3 letter ISO 639-3 codes)",
+        )
+        parser.add_argument(
+            "-s",
+            "--subtitle-lang",
+            required=True,
+            nargs="*",
+            help="list of language codes to keep and in which order. (uses 3 letter ISO 639-3 codes)",
+        )
+        parser.add_argument(
+            "--prefer-text-subtitles",
+            action="store_true",
+            help="text based subtitles will be preferred over image based subtitles",
+        )
+        parser.add_argument(
+            "--prefer-sdh-subtitles",
+            action="store_true",
+            help="hearing imparied subtitles will be preferred",
+        )
+        parser.add_argument(
+            "--undefined-lang",
+            default="eng",
+            help="language code to use if undefined (default: %(default)s)",
         )
         parser.add_argument(
             "--output-directory",
